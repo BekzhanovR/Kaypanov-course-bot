55ba3420220b89d56dc8e897f2a1a00d4a83aabe
BekzahnovR <bekzhanovofficial@gmail.com>
2024-07-01 11:46:05 +0500

    Update

--- main.py
+++ main.py
@@ -23,42 +23,11 @@ dp.middleware.setup(LoggingMiddleware())
 class SurveyStates(StatesGroup):
     name = State()
     phone = State()
-    age = State()
-    city = State()
-    business = State()
-    source = State()
-    course = State()
-    reason = State()
 
 # Boshlang'ich tugmalar
 start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 start_button = KeyboardButton('/start')
 start_keyboard.add(start_button)
-
-# Sorovnoma tugmalari
-age_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-age_buttons = ["18-24", "25-34", "35-44", "44-54", "55-60"]
-age_keyboard.add(*age_buttons)
-
-city_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-city_buttons = ["Nukus shahri", "Xojeli", "Taxiyatash", "Shimbay", "Qon'irat", "Shomanay", "Qanliko'l", "Kegeyli", "To'rtko'l", "Taxtako'pir", "Moynaq", "Bozataw", "Beruniy", "Amudarya", "No'kis rayon"]
-city_keyboard.add(*city_buttons)
-
-business_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-business_buttons = ["Sovda-sotiq", "Xizmmat ko'rsatish", "Ishlab shiqish", "Ta'lim", "boshqa"]
-business_keyboard.add(*business_buttons)
-
-source_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-source_buttons = ["Telegram", "Instagram", "Facebook", "Websayt", "Boshqa"]
-source_keyboard.add(*source_buttons)
-
-course_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-course_buttons = ["Ha", "Yoq"]
-course_keyboard.add(*course_buttons)
-
-reason_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-reason_buttons = ["Kópten beri kúzeter edim. Sabaqlıqtı kútip júr edim.", "Oqıǵım kelip júr baslanǵısh maǵlıwmatlardı aldım.", "Maǵan biznes boyınsha real keysler unaydı."]
-reason_keyboard.add(*reason_buttons)
 
 # Ma'lumotlar bazasini yaratish va ulash
 conn = sqlite3.connect('survey_data.db')
@@ -68,13 +37,7 @@ CREATE TABLE IF NOT EXISTS survey (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    age TEXT NOT NULL,
-    city TEXT NOT NULL,
-    business TEXT NOT NULL,
-    source TEXT NOT NULL,
-    course TEXT NOT NULL,
-    reason TEXT NOT NULL
+    phone TEXT NOT NULL
 )
 """)
 conn.commit()
@@ -119,7 +82,7 @@ async def process_view_users(callback_query: types.CallbackQuery):
         rows = cursor.fetchall()
         response = "Foydalanuvchilar ro'yxati:\n"
         for row in rows:
-            response += f"ID: {row[0]} \n, Familya ismi: {row[2]} \n, Telefon raqami: {row[3]} \n, Yoshi: {row[4]} \n, Yashash manzilli: {row[5]} \n, Qanday yo'nalishka qiziqadi: {row[6]} \n, Biz haqimizda qayerdan eshitkan: {row[7]} \n, Oldin kursta oqiganmi: {row[8]} \n, Nima uchun bizni tanlagan: {row[9]}\n"
+            response += f"ID: {row[0]} \n, Familya ismi: {row[2]} \n, Telefon raqami: {row[3]}\n"
         await bot.send_message(callback_query.from_user.id, response)
     else:
         await bot.send_message(callback_query.from_user.id, "Siz admin emassiz!")
@@ -146,46 +109,10 @@ async def process_name(message: types.Message, state: FSMContext):
 @dp.message_handler(state=SurveyStates.phone)
 async def process_phone(message: types.Message, state: FSMContext):
     await state.update_data(phone=message.text)
-    await message.reply("Yoshingizni tanlang:", reply_markup=age_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.age)
-async def process_age(message: types.Message, state: FSMContext):
-    await state.update_data(age=message.text)
-    await message.reply("Yashash manzilingizni tanlang:", reply_markup=city_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.city)
-async def process_city(message: types.Message, state: FSMContext):
-    await state.update_data(city=message.text)
-    await message.reply("Qanday biznes turiga qiziqasiz?", reply_markup=business_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.business)
-async def process_business(message: types.Message, state: FSMContext):
-    await state.update_data(business=message.text)
-    await message.reply("Biz haqimizda qayerdan eshitdingiz?", reply_markup=source_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.source)
-async def process_source(message: types.Message, state: FSMContext):
-    await state.update_data(source=message.text)
-    await message.reply("Oldin kurslarimizda o'qidingizmi?", reply_markup=course_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.course)
-async def process_course(message: types.Message, state: FSMContext):
-    await state.update_data(course=message.text)
-    await message.reply("Nima uchun bizni tanladingiz?", reply_markup=reason_keyboard)
-    await SurveyStates.next()
-
-@dp.message_handler(state=SurveyStates.reason)
-async def process_reason(message: types.Message, state: FSMContext):
-    await state.update_data(reason=message.text)
     user_data = await state.get_data()
     
-    cursor.execute("INSERT INTO survey (user_id, name, phone, age, city, business, source, course, reason) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                   (message.from_user.id, user_data['name'], user_data['phone'], user_data['age'], user_data['city'], user_data['business'], user_data['source'], user_data['course'], user_data['reason']))
+    cursor.execute("INSERT INTO survey (user_id, name, phone) VALUES (?, ?, ?)",
+                   (message.from_user.id, user_data['name'], user_data['phone']))
     conn.commit()
     
     inline_kb_video = InlineKeyboardMarkup(row_width=1)
